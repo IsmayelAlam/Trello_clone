13c683d713098ece394ae50fcb3a3304245ba158
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-18 12:29:00 +0600

    add basic style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Board from "./Board";
 
 function App() {
   return (
-    <div className="overflow-hidden">
-      <header className="w-screen bg-slate-500 text-white h-14">
+    <div className="overflow-y-hidden h-screen scrollbar relative">
+      <header className="w-screen fixed bg-slate-500 text-white h-14">
         test project
       </header>
       <Board />
+      <button className="p-1 m-1 block hover:bg-slate-300 text-start rounded">
+        + add new
+      </button>
     </div>
   );
 }
--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,38 @@ import { TackList } from "./TaskList";
 
 export default function Board() {
   return (
-    <div className="w-screen max-h-[screen] bg-slate-700 flex flew">
-      <div className="my-5 mx-2 bg-white rounded-lg shadow-md w-72 max-h-[calc(100vh-15%)] overflow-hidden">
+    <div className="w-fit h-screen bg-slate-700 flex items-start overflow-x-scroll mt-14">
+      <div className="my-5 mx-2 bg-white rounded-lg shadow-md w-72 max-h-[90%] overflow-hidden flex flex-col">
+        <h2 className="text-lg p-2 capitalize pl-5 bg-slate-200">test</h2>
+
+        <TackList num={5} />
+
+        <button className="p-1 m-1 block hover:bg-slate-300 text-start rounded">
+          + add new
+        </button>
+      </div>
+
+      <div className="my-5 mx-2 bg-white rounded-lg shadow-md w-72 max-h-[90%] overflow-hidden flex flex-col">
+        <h2 className="text-lg p-2 capitalize pl-5 bg-slate-200">test</h2>
+
+        <TackList num={10} />
+
+        <button className="p-1 m-1 block hover:bg-slate-300 text-start rounded">
+          + add new
+        </button>
+      </div>
+
+      <div className="my-5 mx-2 bg-white rounded-lg shadow-md w-72 max-h-[90%] overflow-hidden flex flex-col">
+        <h2 className="text-lg p-2 capitalize pl-5 bg-slate-200">test</h2>
+
+        <TackList num={25} />
+
+        <button className="p-1 m-1 block hover:bg-slate-300 text-start rounded">
+          + add new
+        </button>
+      </div>
+
+      <div className="my-5 mx-2 bg-white rounded-lg shadow-md w-72 max-h-[90%] overflow-hidden flex flex-col">
         <h2 className="text-lg p-2 capitalize pl-5 bg-slate-200">test</h2>
 
         <TackList num={50} />
@@ -12,6 +42,30 @@ export default function Board() {
           + add new
         </button>
       </div>
+
+      <div className="my-5 mx-2 bg-white rounded-lg shadow-md w-72 max-h-[90%] overflow-hidden flex flex-col">
+        <h2 className="text-lg p-2 capitalize pl-5 bg-slate-200">test</h2>
+
+        <TackList num={10} />
+
+        <button className="p-1 m-1 block hover:bg-slate-300 text-start rounded">
+          + add new
+        </button>
+      </div>
+
+      <div className="my-5 mx-2 bg-white rounded-lg shadow-md w-72 max-h-[90%] overflow-hidden flex flex-col">
+        <h2 className="text-lg p-2 capitalize pl-5 bg-slate-200">test</h2>
+
+        <TackList num={30} />
+
+        <button className="p-1 m-1 block hover:bg-slate-300 text-start rounded">
+          + add new
+        </button>
+      </div>
+
+      <button className="px-2 py-2 my-5 mx-2 w-72 text-start rounded hover:bg-slate-300">
+        + add new
+      </button>
     </div>
   );
 }
